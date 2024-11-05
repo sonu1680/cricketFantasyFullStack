2d0b365a646f128bc27ec5d7b82d88a7c6d42096
sonu1680 <sonupandit1680@gmail.com>
2024-11-06 00:33:34 +0530

    ...

--- adminDashboard/vite.config.js
+++ adminDashboard/vite.config.js
@@ -2,8 +2,8 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-
 export default defineConfig({
+  base: "/", 
   plugins: [react()],
   resolve: {
     alias: {
